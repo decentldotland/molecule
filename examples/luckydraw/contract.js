5ddfe6f8367bc130196c2ff5e8273cb04ecd5526
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-11-27 00:55:29 +0100

    chore: add `rand` example

---
+++ examples/luckydraw/contract.js
@@ -0,0 +1,32 @@
+export async function handle(state, action) {
+  const input = action.input;
+
+  if (input.function === "draw") {
+    ContractAssert(
+      state.participants.length,
+      "cannot proceed with an empty participants array"
+    );
+    const max = state.participants.length - 1;
+    const drawResult = await _moleculeRandInt(max);
+    state.winners.push({
+      winner: state.participants[drawResult],
+      randInt: drawResult,
+    });
+
+    return { state };
+  }
+
+  async function _moleculeRandInt(max) {
+    try {
+      // min param is assigned to 0 because the draw particiants is an non-empty array
+      const req = await EXM.deterministicFetch(
+        `${state.rand_molecule_endpoint}/generate/0/${max}`
+      );
+      const index = req.asJSON()?.result;
+      ContractAssert(typeof index === "number", "molecule rand error");
+      return index;
+    } catch (error) {
+      throw new ContractError("error molecule rand connection");
+    }
+  }
+}
---
+++ examples/luckydraw/wtf.md
@@ -0,0 +1,22 @@
+## About
+This example contract demonstrates how you can use the `int` atom of `rand` molecule in order to generate a random integer powered by [random.org](https://random.org) and then doing a random lucky draw out a pre-defined `participants` array. 
+
+#### Contract
+- Live deployment: TODO
+- source code: [./contract.js](./contract.js)
+
+## Prerequisites
+
+- EXM SDK
+```console
+npm i -g @execution-machine/sdk
+```
+
+- EXM API token ID: visit [exm.dev](https://exm.dev)
+
+## Iteracting with the contract
+
+#### Example
+```console
+exm function:write $CONTRACT_ID '{"function": "draw"}' --token EXM_TOKEN_ID
+```
