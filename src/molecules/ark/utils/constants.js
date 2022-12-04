d019af7484a0c6a2f53d04c9e68347c4f30b105a
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-12-04 23:49:13 +0100

    feat: add soArk contants utils

--- src/molecules/ark/utils/constants.js
+++ src/molecules/ark/utils/constants.js
@@ -1,1 +1,3 @@
 export const ARK_ORACLE_ADDRESS = `Z7JzRRt2iTQWV5LziNhTV6SP51tVKkCf_qrUqtlwzpg`;
+export const SOARK_API_ENDPOINT = `https://ark-core.decent.land/v2/soark`;
+export const DOMAINS_TYPES = ["ans", "ens", "avvy", "evmos", "lens"];
