3a6ae8e4fd5147469b2d76cfe87bf214047b6a24
Darwin <77340894+charmful0x@users.noreply.github.com>
2023-01-12 19:55:09 +0100

    docs: add `tez-signing` example

---
+++ examples/tez-signing/contract.js
@@ -0,0 +1,44 @@
+export async function handle(state, action) {
+  const input = action.input;
+
+  const names = state.names;
+  const signatures = state.signatures;
+  const verification_message = state.verification_message;
+  const tez_molecule_endpoint = state.tez_molecule_endpoint;
+
+  if (input.function === "register") {
+    const name = input.name;
+    const caller = input.caller;
+    const signature = input.signature;
+
+    ContractAssert(name.trim().length, "error invalid name");
+    ContractAssert(!(name in names), "name already registered");
+    ContractAssert(caller && signature, "missing required arguments");
+    ContractAssert(
+      !signatures.includes(signature),
+      "error signed message used"
+    );
+
+    const message = btoa(verification_message);
+    const res = await _moleculeSignatureVerification(caller, message, signature);
+    state.names[res.address] = `${name.trim()}.tez`;
+    signatures.push(signature);
+
+    return { state };
+  }
+
+  async function _moleculeSignatureVerification(caller, message, signature) {
+    try {
+      const isValid = await EXM.deterministicFetch(
+        `${tez_molecule_endpoint}/tez-auth/${caller}/${message}/${signature}`
+      );
+      ContractAssert(isValid.asJSON()?.result, "unauthorized caller");
+      return isValid.asJSON();
+    } catch (error) {
+      throw new ContractError("molecule res error");
+    }
+  }
+}
+
+
+
